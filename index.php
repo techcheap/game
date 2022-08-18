d225a4258c1bc007e4bc9acdae8dc0b28e2c76d1
techcheap <ometark@outlook.com>
2022-08-18 16:15:15 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+    header('Content-Type: application/json');
+ 
+    if (!isset($_POST['user']) || !isset($_POST['password'])){
+        echo json_encode('User or password is not set.');
+        exit;
+    }
+ 
+    $user = $_POST['user'];
+    $password = $_POST['password'];
+ 
+    $data_arr = array(
+        'is_authorized'=>false,
+        'other_data'=>array(
+        'money'=>10000,
+        'level'=>99,
+        'bonus'=>false,
+        'admin'=>true,
+    ));
+ 
+    if ($user == 'execoder' && $password == '123456789'){
+        $data_arr['is_authorized']=true;
+    }
+ 
+    if ($data_arr['is_authorized']){
+        echo json_encode($data_arr);
+    }
+?>
