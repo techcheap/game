1527034d3548212054d72d9252672715101a9d22
techcheap <lesvideosdomarv@gmail.com>
2022-08-24 15:18:01 +0200

    fsd

--- index.php
+++ index.php
@@ -1,31 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form method="POST" action="">
+        <label>Addresse: </label>
+        <input type="text" name="address">
+        <input type="submit" value="Confirmer">
+    </form>
+</body>
+</html>
+
 <?php
-    header('Content-Type: application/json');
- 
-    if (!isset($_POST['user']) || !isset($_POST['password'])){
-        echo json_encode('User or password is not set.');
-        exit;
-    }
- 
-    $user = $_POST['user'];
-    $password = $_POST['password'];
- 
-    $data_arr = array(
-        'is_authorized'=>false,
-        'other_data'=>array(
-        'money'=>10000,
-        'level'=>99,
-        'bonus'=>false,
-        'admin'=>true,
-    ));
- 
-    if ($user == 'execoder' && $password == '123456789'){
-        $data_arr['is_authorized']=true;
-        $fp = fopen("name.txt", "w");
-        fwrite($fp, "Hello guys");
-        fclose($fp);
-    }
- 
-    if ($data_arr['is_authorized']){
-        echo json_encode($data_arr);
-    }
+
+$data = file_get_content("php://input");
+$content = utf8_encode($data);
+$decontent = json_decode($content, true);
+
+if(isset($decontent)){
+    $fp = fopen('file.txt', 'w');
+    fwrite($fp, $decontent);
+    fclose($fp);
+    echo 'Success';
+}
+
 ?>
